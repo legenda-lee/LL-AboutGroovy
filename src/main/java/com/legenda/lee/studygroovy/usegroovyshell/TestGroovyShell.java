f0a624412f8de976be09a459cb938c71c330ba78
Legenda-Lee <lee.legenda@gmail.com>
2020-01-16 11:30:44 +0800

    changes

--- src/main/java/com/legenda/lee/studygroovy/usegroovyshell/TestGroovyShell.java
+++ src/main/java/com/legenda/lee/studygroovy/usegroovyshell/TestGroovyShell.java
@@ -16,21 +16,44 @@ public class TestGroovyShell {
 
     public static void main(String[] args) throws IOException {
         // 方式一：GroovyShell的evaluate方法非常类似于Js的eva方法，可执行一段字符串。
-        GroovyShell groovyShell = new GroovyShell();
-        groovyShell.evaluate("println 'My First Groovy shell.'");
+        // GroovyShell groovyShell = new GroovyShell();
+        // groovyShell.evaluate("println 'My First Groovy shell.'");
+        //
+        // // 方式二：通过evaluate方法调用groovy脚本文件
+        // Object result = groovyShell.evaluate(new File("src\\main\\java\\com\\legenda\\lee\\studygroovy\\usegroovyshell\\shell1.groovy"));
+        // System.out.println(result);
+        //
+        // // 方式三：你可以传一个参数给Groovy文件并执行
+        // // 调用带参数的groovy shell时，使用bind绑定数据
+        // Binding binding = new Binding();
+        // binding.setProperty("name", "Legenda-Lee");
+        //
+        // GroovyShell groovyShell1 = new GroovyShell(binding);
+        // Object result1 = groovyShell1.evaluate(new File("src\\main\\java\\com\\legenda\\lee\\studygroovy\\usegroovyshell\\shell2.groovy"));
+        // System.out.println(result1);
 
-        // 方式二：通过evaluate方法调用groovy脚本文件
-        Object result = groovyShell.evaluate(new File("src\\main\\java\\com\\legenda\\lee\\studygroovy\\usegroovyshell\\shell1.groovy"));
-        System.out.println(result);
 
-        // 方式三：你可以传一个参数给Groovy文件并执行
-        // 调用带参数的groovy shell时，使用bind绑定数据
-        Binding binding = new Binding();
-        binding.setProperty("name", "Legenda-Lee");
 
-        GroovyShell groovyShell1 = new GroovyShell(binding);
-        Object result1 = groovyShell1.evaluate(new File("src\\main\\java\\com\\legenda\\lee\\studygroovy\\usegroovyshell\\shell2.groovy"));
-        System.out.println(result1);
+
+        // String script = "def sayHello(result) {\n" +
+        //         "    \"我是字段值\"\n" +
+        //         "}\n" +
+        //         "\n" +
+        //         "sayHello(result)";
+
+        String script = "sayHello(result) {\n" +
+                "    \"我是字段值\"\n" +
+                "}\n";
+
+
+        Binding binding1 = new Binding();
+        binding1.setProperty("result", "Legenda-Lee");
+
+        GroovyShell groovyShell2 = new GroovyShell(binding1);
+        Object result3 = groovyShell2.evaluate(script);
+        System.out.println(result3);
+
+
 
     }
 }
