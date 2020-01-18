5389ff88303c2d3bc3b56518d81dd26e0c7f8a72
Legenda-Lee <lee.legenda@gmail.com>
2020-01-18 14:34:24 +0800

    changes

--- src/main/java/com/legenda/lee/studygroovy/usegroovyshell/shell2.groovy
+++ src/main/java/com/legenda/lee/studygroovy/usegroovyshell/shell2.groovy
@@ -3,3 +3,13 @@ sayHello(name) {
 }
 
 sayHello(name)
+
+getIpCity(result) {
+    String city = null;
+    JSONObject jo = JSONObject.parseObject(result);
+    if ("0".equals(jo.getString("code"))) {
+        JSONObject data = jo.getJSONObject("data");
+        city = data.getString("city");
+    }
+    return city;
+}
